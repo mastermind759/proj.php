8f3ef58bda355982358149a0fd11de85c46b7a25
mastermind759 <38043682+mastermind759@users.noreply.github.com>
2021-01-08 02:14:00 +0530

    Add files via upload

---
+++ Bookseller/assets/includes/footer.php
@@ -0,0 +1,11 @@
+<?php
+
+
+?>
+<link rel="stylesheet" type="text/css" href="../css/footer.css">
+<footer class="foot">
+  <div class=>
+    <span class="text-muted"><h2>contact us at    mindtools.io</h2></span>
+    
+  </div>
+</footer>
---
+++ Bookseller/assets/includes/navigation_bar.php
@@ -0,0 +1,16 @@
+<?php 
+
+?>
+<link rel="stylesheet" type="text/css" href="../css/nav.css">
+<div class="nav">
+		<div class="logo">
+			LOGO
+		</div>
+		<ul class="list">
+			<li> <a href="#href">MY ACCOUNT</a></li>
+			<li><a  href="#href">CART</a></li>
+		</ul>
+	</div>
+
+
+
